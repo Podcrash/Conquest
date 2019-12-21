9129cc27de1dbc9e3fb0de7143eee33792b9518a
rain474 <3-lordraindance@users.noreply.ccl.pdcl.club>
2019-12-21 00:25:03 -0500

    phantom dash implemented

---
+++ src/main/java/me/raindance/champions/kits/classes/Thief.java
@@ -0,0 +1,44 @@
+package me.raindance.champions.kits.classes;
+
+import com.podcrash.api.mc.effect.status.Status;
+import com.podcrash.api.mc.effect.status.StatusApplier;
+import me.raindance.champions.kits.ChampionsPlayer;
+import me.raindance.champions.kits.Skill;
+import me.raindance.champions.kits.enums.SkillType;
+import com.podcrash.api.mc.sound.SoundWrapper;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class Assassin extends ChampionsPlayer {
+    public Assassin(Player player, List<Skill> skills) {
+        super(player);
+        this.skills = skills;
+        setFallDamage(1.5d);
+        setSound(new SoundWrapper("random.bow", 0.95F, 126));
+        this.armor = new Material[]{Material.LEATHER_BOOTS, Material.LEATHER_LEGGINGS, Material.LEATHER_CHESTPLATE, Material.LEATHER_HELMET};
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public String getName() {
+        return "Assassin";
+    }
+    public SkillType getType() {
+        return SkillType.Assassin;
+    }
+
+    @Override
+    public void respawn() {
+        super.respawn();
+        StatusApplier.getOrNew(getPlayer()).applyStatus(Status.SPEED, Integer.MAX_VALUE, 1, true, true);
+    }
+
+    @Override
+    public int getHP() {
+        return 0;
+    }
+}
---
+++ src/main/java/me/raindance/champions/kits/skills/rogue/PhantomDash.java
@@ -0,0 +1,124 @@
+package me.raindance.champions.kits.skills.rogue;
+
+import com.podcrash.api.mc.damage.Cause;
+import com.podcrash.api.mc.damage.DamageApplier;
+import me.raindance.champions.kits.annotation.SkillMetadata;
+import me.raindance.champions.kits.enums.InvType;
+import me.raindance.champions.kits.enums.ItemType;
+import me.raindance.champions.kits.enums.SkillType;
+import me.raindance.champions.kits.iskilltypes.action.ICooldown;
+import me.raindance.champions.kits.skilltypes.Instant;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EnderPearl;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.util.Vector;
+
+/**
+ * Phantom Dash
+ * Class: Rogue
+ * Type: Axe Ability
+ * Cooldown: 10 seconds
+ * Description: Right click your axe to activate. Launch a Phantom Pearl at target direction.
+ * If it hits a player, you teleport to them, dealing 5 damage if they are an enemy.
+ * Details:
+ * Phantom Pearls are ender pearls.
+ * The pearl emits the particles of an ender pearl landing when it lands.
+ */
+
+@SkillMetadata(skillType = SkillType.Rogue, invType = InvType.AXE)
+public class PhantomDash extends Instant implements ICooldown {
+    //We are using the instant base class to format this
+
+    //we are going to be storing our own instance of the pearl here to reference it in another event.
+    private EnderPearl pearl; //nevermind
+
+    @Override
+    protected void doSkill(PlayerInteractEvent event, Action action) {
+        //if the action isn't right click or if there is on cooldowm
+        if(rightClickCheck(action) || onCooldown()) return;
+
+        //Set the cooldown
+        setLastUsed(System.currentTimeMillis());
+
+        //Get the direction of the player because we need to launch projectile
+        Location currentLocOfPlayer = getPlayer().getLocation();
+        //These are slightly different, as noted by the name of the functions
+        Location launchLocation = getPlayer().getEyeLocation();
+
+        Vector direction = currentLocOfPlayer.getDirection();
+        World world = currentLocOfPlayer.getWorld();
+
+        //spawn the enderpearl, we may need custom of these classes but for now this is fine.
+        this.pearl = world.spawn(launchLocation, EnderPearl.class);
+        //we will also be assuming that the pearl doesn't have a velocity when it spawns.
+
+        Vector mulitplied = direction.multiply(4.2F); //magic number
+        pearl.setVelocity(mulitplied);
+
+        //AT THIS POINT: we have spawned the ender pearl.
+    }
+
+    @EventHandler
+    public void enderPearlHit(EntityDamageByEntityEvent event) {
+        //checks
+        Entity damager = event.getDamager();
+
+        //if the damager is not a pearl or the pearl we need, return.
+        //if(!(damager instanceof EnderPearl) || damager != this.pearl) return;
+        //actually, just do this: check if the pearl is not the damager
+        if(damager != this.pearl) return;
+        Entity victim = event.getEntity();// victim
+
+        //we want to check if the damager hit is an actual living damager and not something random (like item frames)
+        if(!(victim instanceof LivingEntity)) return;
+
+        //at this point, the pearl has hit some living damager,
+        //so we need to do 5 damage and have the user teleport.
+
+        //damage
+        DamageApplier.damage((LivingEntity) victim, getPlayer(), 5, this, false);
+
+        //teleport
+        getPlayer().teleport(damager.getLocation());
+    }
+
+    /**
+     * The forums r g0d: https://bukkit.org/threads/how-to-disable-enderpearl-teleport.128993/
+     */
+    @EventHandler
+    public void disablePearlTeleport(PlayerTeleportEvent e) {
+        //we want to make sure that everyone doesn't get blocked
+        if(e.getPlayer() != getPlayer()) return;
+
+        //we also want to make sure that pearls don't get blocked if you are not using this skill
+        if(e.getCause() != PlayerTeleportEvent.TeleportCause.ENDER_PEARL) return;
+
+        //cancel
+        e.setCancelled(true);
+    }
+
+    //Generator methods for this
+    @Override
+    public float getCooldown() {
+        return 10;
+    }
+
+
+    @Override
+    public String getName() {
+        return "Phantom Dash";
+    }
+
+    @Override
+    public ItemType getItemType() {
+        return ItemType.AXE;
+    }
+}
