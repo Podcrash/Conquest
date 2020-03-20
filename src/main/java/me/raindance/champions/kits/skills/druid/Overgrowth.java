79888a79c55b10592c095ca3f10b1b616f5dddd7
poetahto <epic.potat@gmail.com>
2020-03-19 19:51:48 -0700

    added audio + visual cue to overgrowth, added a message for it not affecting enemies

--- src/main/java/me/raindance/champions/kits/skills/druid/Overgrowth.java
+++ src/main/java/me/raindance/champions/kits/skills/druid/Overgrowth.java
@@ -1,7 +1,11 @@
 package me.raindance.champions.kits.skills.druid;
 
+import com.abstractpackets.packetwrapper.WrapperPlayServerWorldParticles;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.podcrash.api.mc.effect.particle.ParticleGenerator;
 import com.podcrash.api.mc.effect.status.Status;
 import com.podcrash.api.mc.effect.status.StatusApplier;
+import com.podcrash.api.mc.sound.SoundPlayer;
 import me.raindance.champions.kits.annotation.SkillMetadata;
 import me.raindance.champions.kits.enums.InvType;
 import me.raindance.champions.kits.enums.ItemType;
@@ -9,6 +13,8 @@ import me.raindance.champions.kits.enums.SkillType;
 import me.raindance.champions.kits.iskilltypes.action.ICooldown;
 import me.raindance.champions.kits.iskilltypes.action.IEnergy;
 import me.raindance.champions.kits.skilltypes.Interaction;
+import net.md_5.bungee.protocol.packet.Chat;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 
 @SkillMetadata(id = 206, skillType = SkillType.Druid, invType = InvType.SWORD)
@@ -20,8 +26,15 @@ public class Overgrowth extends Interaction implements ICooldown, IEnergy {
     @Override
     public void doSkill(LivingEntity clickedEntity) {
         if(onCooldown()) return;
-        if(!isAlly(clickedEntity)) return;
+        if(!isAlly(clickedEntity)) {
+            getPlayer().sendMessage(String.format("%sSkill> %sOvergrowth %sdoes not affect your enemies.", ChatColor.BLUE, ChatColor.GREEN, ChatColor.GRAY));
+            return;
+        }
         StatusApplier.getOrNew(clickedEntity).applyStatus(Status.ABSORPTION, 5, 1);
+        WrapperPlayServerWorldParticles packet = ParticleGenerator.createParticle(clickedEntity.getLocation().toVector(), EnumWrappers.Particle.HEART,
+                3, 0, 0.9f, 0);
+        getPlayer().getWorld().getPlayers().forEach(p -> ParticleGenerator.generate(p, packet));
+        SoundPlayer.sendSound(getPlayer(), "mob.enderdragon.wings", 0.8F, 1);
         setLastUsed(System.currentTimeMillis());
     }
 
