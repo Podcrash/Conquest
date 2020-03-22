233816f353f7932b2995f520deed5f80f0e6bb4f
poetahto <epic.potat@gmail.com>
2020-03-21 17:41:21 -0700

    updated the hardcoded class descriptions and made disarmed users deal no kb

--- src/main/java/me/raindance/champions/inventory/ChampionsInventory.java
+++ src/main/java/me/raindance/champions/inventory/ChampionsInventory.java
@@ -21,62 +21,71 @@ public class ChampionsInventory {
 
         addClass(SkillType.Warden, Material.IRON_HELMET,
                 "Wardens function as off-tanks, boasting",
-                "damage-soaking capabilities and crowd control.",
-                "While they lack mobility, they make up for it with",
-                "their considerable skirmishing ability.");
+                "damage-soaking capabilities and crowd",
+                "control. While they lack mobility, they",
+                "make up for it with their considerable",
+                "skirmishing ability.");
 
         addClass(SkillType.Duelist, Material.DIAMOND_SWORD,
-                "Duelists excel in fighting enemies one by one",
-                "and dealing consistent single-target damage.",
-                "They primarily rely on their melee attacks",
-                "to cut their opponents down.");
+                "Duelists excel in fighting enemies one",
+                "by one and dealing consistent single-target",
+                "damage. They primarily rely on their melee",
+                "attacks to cut their opponents down.");
 
         addClass(SkillType.Vanguard, Material.DIAMOND_CHESTPLATE,
-                "Vanguards are resilient and powerful teamfighters.",
-                        "They sacrifice damage in exchange for strong crowd ",
-                        "control abilities and high durability.");
+                "Vanguards are resilient and powerful",
+                        "teamfighters. They sacrifice damage in",
+                        "exchange for strong crowd control",
+                        "abilities and high durability.");
         
         addClass(SkillType.Berserker, Material.DIAMOND_AXE, 
-                "Berserkers are mobile, close-range fighters capable",
-                        "of dealing with multiple enemies. Though somewhat",
-                        "frail, they can become a huge threat in teamfights",
-                        "if left unchecked. ");
+                "Berserkers are mobile, close-range",
+                        "fighters capable of dealing with multiple",
+                        "enemies. Though somewhat frail, they can",
+                        "become a huge threat in teamfights if left",
+                        "unchecked.");
 
         addClass(SkillType.Marksman, Material.BOW, 
-                "Marksmen rely on their precise, long-ranged attacks",
-                        "to support their team. They exclusively use their bow",
-                        "to deal heavy damage and cripple their targets.");
+                "Marksmen rely on their precise, long-ranged",
+                        "attacks to support their team. They exclusively",
+                        "use their bow to deal heavy damage and",
+                        "cripple their targets.");
 
         addClass(SkillType.Hunter, Material.BONE,
-                "Hunters are nimble archers that shine in both",
-                        "short-range and mid-range engagements. They are able",
-                        "to use their bow to keep strong enemies at a distance",
-                        "and to secure kills on the weak.\n");
+                "Hunters are nimble archers that shine in",
+                        "both short-range and mid-range engagements.",
+                        "They are able to use their bow to keep strong",
+                        "enemies at a distance and to secure kills on",
+                        "the weak.");
 
         addClass(SkillType.Sorcerer, Material.BLAZE_ROD,
-                "Hunters are nimble archers that shine in both",
-                        "short-range and mid-range engagements. They are able",
-                        "to use their bow to keep strong enemies at a distance",
-                        "and to secure kills on the weak.");
+                "Sorcerers depend on their abilities to",
+                        "dish out damage from a range. They are",
+                        "exceptional at whittling down their opponents",
+                        "over time and setting up potential kills with",
+                        "their spells.");
 
         addClass(SkillType.Druid, Material.SAPLING,
-                "Druids act as supports for their team, empowering their",
-                        "allies with enchantments and protecting them with healing magic. ",
-                        "In addition, they offer a substantial amount of utility in ",
-                        "teamfights with their crowd control.");
+                "Druids act as supports for their team, empowering",
+                        "their allies with enchantments and protecting",
+                        "them with healing magic. In addition, they offer",
+                        "a substantial amount of utility in team-fights with",
+                        "their crowd control.");
 
         addClass(SkillType.Rogue, Material.LEATHER_BOOTS,
-                "Rogues are agile assassins who specialize in taking down",
-                        "fragile priority targets. They depend on their mobility to",
-                        "close the gap between their enemies and their high burst ",
-                        "damage to dispatch of them quickly.");
+                "Rogues are agile assassins who specialize in",
+                        "taking down fragile priority targets. They depend",
+                        "on their mobility to close the gap between their",
+                        "enemies and their high burst damage to dispatch",
+                        "them quickly.");
 
         addClass(SkillType.Thief, Material.COAL, 
-                "Thieves are cunning and elusive, adept at disorienting",
-                        "their adversaries with a plethora of tools and abilities.",
-                        "Rather than eliminating the opposition themselves, they",
-                        "aim to distract them for as long as possible while their",
-                        "allies capitalize off the confusion.");
+                "Thieves are cunning and elusive, adept at",
+                        "disorienting their adversaries with a plethora of",
+                        "tools and abilities. Rather than eliminating the",
+                        "opposition themselves, they aim to distract them",
+                        "for as long as possible while their allies capitalize",
+                        "off the confusion.");
         /*
         ItemStack assassin = ItemStackUtil.createItem(Material.LEATHER_HELMET, "Assassin", Arrays.asList("Use stealth hacks", "and insane mobility to kill every1"));
         ItemStack brute = ItemStackUtil.createItem(Material.DIAMOND_HELMET, "Brute", Arrays.asList("Use crowd control", "and filth to kill every1"));
