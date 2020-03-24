e5101c7f249819fd7500233b0db507abe4da56c0
rain474 <lordraindance5@gmail.com>
2020-03-24 19:29:11 -0400

    made traps disappear after death, kit reapply, and time

--- src/main/java/me/raindance/champions/kits/itemskill/TrapItem.java
+++ src/main/java/me/raindance/champions/kits/itemskill/TrapItem.java
@@ -1,26 +1,36 @@
 package me.raindance.champions.kits.itemskill;
 
 import com.podcrash.api.mc.callback.helpers.TrapSetter;
+import com.podcrash.api.mc.callback.sources.AwaitTime;
+import com.podcrash.api.mc.events.DeathApplyEvent;
 import com.podcrash.api.mc.events.TrapPrimeEvent;
 import com.podcrash.api.mc.events.TrapSnareEvent;
 import com.podcrash.api.mc.game.GameManager;
+import me.raindance.champions.events.ApplyKitEvent;
 import me.raindance.champions.kits.itemskill.IItem;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public abstract class TrapItem implements IItem, Listener {
     private Map<Integer, String> itemOwners;
     private long delay;
+
+    protected long despawnDelay;
     public TrapItem(long delay) {
         this.itemOwners = new HashMap<>();
         this.delay = delay;
+
+        this.despawnDelay = 20L * 1000L;
     }
 
     protected abstract Item throwItem(Player player, Action action);
@@ -40,6 +50,10 @@ public abstract class TrapItem implements IItem, Listener {
         Item item = e.getItem();
         if(!itemOwners.containsKey(item.getEntityId())) return;
         primeTrap(item);
+        AwaitTime time = new AwaitTime(despawnDelay);
+        time.then(() -> {
+            TrapSetter.destroyTrap(item);
+        }).runAsync(5, 0);
     }
 
     @EventHandler
@@ -52,5 +66,36 @@ public abstract class TrapItem implements IItem, Listener {
         if(GameManager.getGame() != null && GameManager.getGame().isRespawning(snared)) {
             e.setCancelled(true);
         }else snareTrap(owner, snared, item);
+    }
+
+    @EventHandler
+    public void death(DeathApplyEvent e) {
+        consumeAllTrapItems(e.getPlayer(), TrapSetter::destroyTrap);
+    }
+
+    @EventHandler
+    public void apply(ApplyKitEvent e) {
+        Player player = e.getChampionsPlayer().getPlayer();
+        consumeAllTrapItems(player, TrapSetter::destroyTrap);
+    }
+
+    public void consumeAllTrapItems(Player player, Consumer<Item> consumer) {
+        World world = player.getWorld();
+        Collection<Item> items = world.getEntitiesByClass(Item.class);
+        for (Map.Entry<Integer, String> entry : itemOwners.entrySet()) {
+            int id = entry.getKey();
+            String ownerName = entry.getValue();
+            if (!ownerName.equalsIgnoreCase(player.getName())) continue;
+
+            Item i = null;
+            for (Item item : items) {
+                if (item.getEntityId() == id) {
+                    i = item;
+                    break;
+                }
+            }
+            if(i == null) break;
+            consumer.accept(i);
+        }
     }
 }
