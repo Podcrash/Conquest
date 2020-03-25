a762b06860da1333f6b703dcb5d183e9b39fd430
poetahto <epic.potat@gmail.com>
2020-03-24 20:56:34 -0700

    slightly edited ViewCommand to eliminate the need for a gameId

--- src/main/java/me/raindance/champions/commands/ViewCommand.java
+++ src/main/java/me/raindance/champions/commands/ViewCommand.java
@@ -1,6 +1,7 @@
 package me.raindance.champions.commands;
 
 import com.podcrash.api.mc.game.GameManager;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -8,13 +9,12 @@ public class ViewCommand extends CommandBase{
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(args.length == 1) {
-            int id = Integer.parseInt(args[0]);
-            if(GameManager.getGame() != null) {
-                sender.sendMessage(GameManager.getGame().toString());
-            }else sender.sendMessage("Game with ID " + id + " does not exist!");
-            return true;
-        }
-        return false;
+        if(GameManager.getGame() != null) {
+            sender.sendMessage(GameManager.getGame().toString());
+        }else sender.sendMessage(String.format(
+                "%sConquest> %sA game has not been created yet.",
+                ChatColor.BLUE,
+                ChatColor.GRAY));
+        return true;
     }
 }
