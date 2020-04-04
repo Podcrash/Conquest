f3ead9d9564da98f2284d04a27e05ecaefb85a1d
poetahto <epic.potat@gmail.com>
2020-04-03 23:33:32 -0700

    cleaned up chained hook, added many comments

--- src/main/java/me/raindance/champions/kits/skills/vanguard/ChainedHook.java
+++ src/main/java/me/raindance/champions/kits/skills/vanguard/ChainedHook.java
@@ -4,7 +4,6 @@ import com.abstractpackets.packetwrapper.WrapperPlayServerWorldParticles;
 import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.podcrash.api.mc.damage.DamageApplier;
 import com.podcrash.api.mc.events.ItemCollideEvent;
-import me.raindance.champions.Main;
 import com.podcrash.api.mc.effect.particle.ParticleGenerator;
 import com.podcrash.api.mc.item.ItemManipulationManager;
 import me.raindance.champions.kits.annotation.SkillMetadata;
@@ -16,93 +15,161 @@ import com.podcrash.api.mc.sound.SoundPlayer;
 import com.podcrash.api.mc.sound.SoundWrapper;
 import com.podcrash.api.mc.util.VectorUtil;
 import me.raindance.champions.kits.skilltypes.Instant;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Vector;
 
+// The skillMetadata is pretty self explanatory, the only thing you need to watch out for is making sure that there
+// are no repeated ID's! The ID of a skill is vital to making sure kits are saved correctly, so avoid duplicates.
+// As for the exact number you should choose, its pretty much up to you. Look through the other skills for the class,
+// usually there is some sort of rough pattern to follow.
 @SkillMetadata(id = 801, skillType = SkillType.Vanguard, invType = InvType.SHOVEL)
 public class ChainedHook extends Instant implements ICooldown {
+    // This currentItemID variable allows us to keep track of what chained hook item is actually ours: it's use
+    // becomes more apparent in the collideItem() method.
     private int currentItemID;
-    private float vect;
 
+    // These three variables could be replaced into the code, but we leave them out here to make implementing balance
+    // changes much more easier (we don't have to dig through the code to change stuff; this is ideal).
+    private float cooldown = 10;
+
+    private float itemSpeedMult = 1.9f;
+    private double itemVerticalBoost = 0.27;
+    private float victimSpeedMult = 1.6f;
+    private double victimVerticalBoost = 0.65;
+
+    private double victimVerticalBoostLimit = 1.35;
+    private double hitboxRadius = 2.25;
+
+    // This method should always return the name of the skill that we want to be displayed. It's necessary whenever we
+    // make a new skill.
     @Override
     public String getName() {
         return "Chained Hook";
     }
 
+    // This method should specify what item must be used to active this skill. Note the differences between the ItemType and
+    // InvType enums; it is subtle but important.
     @Override
     public ItemType getItemType() {
         return ItemType.SHOVEL;
     }
 
-    public ChainedHook() {
-        super();
-        this.vect = 1.1F + 0.3F * 5;
+    // All skills must have this method, unless they are a passive or something. It lets the game know how long it
+    // should wait before allowing the player to use it again, but you probably already knew that.
+    @Override
+    public float getCooldown() {
+        return cooldown;
     }
 
+    // Your standard constructor for the class, normal stuff.
+    public ChainedHook() {
+        super();
+    }
+
+    // For abilities that extend the Instant skilltype, the method doSkill() will be called whenever the player clicks
+    // (right or left). We still activate this method for left clicks because of skills similar to glacial blade (which would
+    // fall under the category of instant).
     @Override
     protected void doSkill(PlayerEvent event, Action action) {
+        // Make sure we aren't on cool down, and that we actually right clicked.
         if(!rightClickCheck(action) || onCooldown()) return;
+        // Now that we are sure the player can and wants to use the skill, tell the system that we have -officially- used the skill.
+        // This is really important because it starts the cool down for when this skill can be used again.
         setLastUsed(System.currentTimeMillis());
+
+        // The release method handles creating the projectile, and what we should do if it hits someone. It could be
+        // named whatever, the name 'release' is just a remnant from when this skill was a ChargeUp.
         release();
 
+        // Send the 'You used' message blah blah blah (easier than typing it out every time).
         getPlayer().sendMessage(getUsedMessage());
     }
 
-    public void release() {
+    private void release() {
+        // Create a new vector based on where the player is looking.
         Vector vector = getPlayer().getLocation().getDirection();
-        double charge = 0.8D;
-        Vector itemVector = vector.clone().normalize().multiply(this.vect/1.25F);
-        Location oldLocation = getPlayer().getLocation();
-        itemVector = itemVector.setY(itemVector.getY() + 0.3).multiply(0.5F + 0.5F * charge);
+        // Normalize the vector (aka change the magnitude to one) before multiplying by our speed multiplier.
+        Vector itemVector = vector.clone().normalize().multiply(itemSpeedMult);
+        // Give the item a small boost in the Y direction so that it follows more of an arc
+        itemVector = itemVector.setY(itemVector.getY() + itemVerticalBoost);
+
+        Location attackerLocation = getPlayer().getLocation();
+
+        // Create a new item at the attacker's eye height with the initial velocity that we defined earlier.
         Item spawnItem = ItemManipulationManager.regular(Material.TRIPWIRE_HOOK, getPlayer().getEyeLocation(), itemVector);
+        // Save this item's ID (which is the actual 'flesh hook' that the attacker throws) so that later in the
+        // collideItem() method we can check to see if OUR specific flesh hook (defined by its ID) has hit the the victim.
         this.currentItemID = spawnItem.getEntityId();
-        ItemManipulationManager.intercept(spawnItem, 2.25,
+
+        // Everything below will be run when the item spawnItem collides with something (a player, a block ect)
+        ItemManipulationManager.intercept(spawnItem, hitboxRadius,
                 ((item, entity, land) -> {
-                    if (entity == getPlayer()) { return; }
+                    // First, we remove the item because we have hit something. The remove() method will physically
+                    // remove the item from the world, but since we know where it hit and everything, this is ok.
                     item.remove();
+                    // If its not an entity we don't really care
                     if(entity == null) return;
-                    double amnt = 0.20F + 0.8F * (charge/1.5D);
-                    Location away = entity.getLocation();
-                    Vector newVect = VectorUtil.fromAtoB(away, oldLocation);
-                    newVect.normalize().multiply(this.vect).multiply(amnt);
-                    double addY = newVect.getY() + 0.4F  + 0.4F * amnt;
-                    if(addY > 1.35F) addY = 1.35F;
-                    entity.setVelocity(newVect.setY(addY));
+
+                    Location victimLocation = entity.getLocation();
+                    // Here we find the general direction to which the victim should be pulled; the method fromAtoB()
+                    // just subtracts the first vector from the second one, and then we normalize it so that the actual
+                    // distance between the two players doesn't affect the pull strength.
+                    Vector victimVector = VectorUtil.fromAtoB(victimLocation, attackerLocation).normalize();
+                    // Now we multiply the victim's resultant velocity by the multiplier so we can change how
+                    // strongly they will be pulled.
+                    victimVector.multiply(victimSpeedMult);
+                    // This line of code is really similar to what we did with the item's velocity, but now we are
+                    // limiting the power of the vertical boost to our defined limit. Math.min(x, y) will always
+                    // return the smallest of the two inputted values, so its nice for establishing limits.
+                    victimVector.setY(Math.min(victimVector.getY() + victimVerticalBoost, victimVerticalBoostLimit));
+
+                    // Now we can finally pull the entity that we hit towards us.
+                    entity.setVelocity(victimVector);
+
+                    // Play the nice sound and apply damage to those noobs who did not train long enough in Diradus-1.
                     SoundPlayer.sendSound(getPlayer(), "random.successful_hit", 0.75F, 50);
                     DamageApplier.damage(entity, getPlayer(), 6, this, false);
                 }));
+        // This makes sure the player can't pick up the item.
+        // (I'm not actually 100% sure if it's necessary, probably better to be safe).
         spawnItem.setPickupDelay(1000);
+
+        // These lines of code essentially grab the ItemMeta of our chained hook item, and change it's name property
+        // to something unique. Why do we do this? So the item won't stack with other items, usually more of a problem
+        // with skills similar to inferno.
         ItemMeta meta = spawnItem.getItemStack().getItemMeta();
         meta.setDisplayName(Long.toString(System.currentTimeMillis()));
         spawnItem.getItemStack().setItemMeta(meta);
 
+        // This plays the sound when the skill is used, not when it hits someone. Sounds can be found here:
+        // https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/mapping-and-modding-tutorials/2213619-1-8-all-playsound-sound-arguments
+        // volume is what you would expect, and for some reason 63 pitch means that the sound will stay the same. If you
+        // are testing the sounds with /playsound in game, this essentially means 63 = 1, 126 = 2 and so on.
         SoundWrapper sound = new SoundWrapper("fire.ignite", 0.8F, 70);
-        WrapperPlayServerWorldParticles packet = ParticleGenerator.createParticle(EnumWrappers.Particle.CRIT, 2);
 
+        // ParticleGenerator.generateEntity() is a really cool and fun method that will cause the specified particles to
+        // follow an entity that we provide. We can also provide it with a sound to play as it follows the entity as well!
+        WrapperPlayServerWorldParticles packet = ParticleGenerator.createParticle(EnumWrappers.Particle.CRIT, 2);
         ParticleGenerator.generateEntity(spawnItem, packet, sound);
     }
 
+    // This method will be run whenever our item collides with something. ItemCollideEvent is called right before our
+    // intercept method (handled above) does all the heavy calculations, so it's more efficient to cancel it here on
+    // certain occasions before we waste our time.
     @EventHandler
     public void collideItem(ItemCollideEvent e) {
+        // If another ability has caught this event and canceled it before us, we will respect that and not allow the
+        // event to finish.
         if(e.isCancelled()) return;
-        //identity check + owner of item check = cancel collision
+        // Now we make sure that we didn't hit ourselves with our own item.
         if(e.getCollisionVictim() == getPlayer() && e.getItem().getEntityId() == currentItemID)
             e.setCancelled(true);
     }
-
-    @Override
-    public float getCooldown() {
-        return 10;
-    }
-
 }
 
