726236ec50a4d0874408eef3ef80c16483840f31
poetahto <epic.potat@gmail.com>
2020-04-07 18:37:33 -0700

    added the gust skill, based on pyro's idea

---
+++ src/main/java/me/raindance/champions/kits/skills/sorcerer/Gust.java
@@ -0,0 +1,98 @@
+package me.raindance.champions.kits.skills.sorcerer;
+
+import com.abstractpackets.packetwrapper.WrapperPlayServerWorldParticles;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.podcrash.api.mc.damage.DamageApplier;
+import com.podcrash.api.mc.effect.particle.ParticleGenerator;
+import com.podcrash.api.mc.effect.status.Status;
+import com.podcrash.api.mc.effect.status.StatusApplier;
+import com.podcrash.api.mc.sound.SoundPlayer;
+import com.podcrash.api.mc.util.PacketUtil;
+import com.podcrash.api.mc.util.VectorUtil;
+import com.podcrash.api.mc.world.BlockUtil;
+import me.raindance.champions.kits.annotation.SkillMetadata;
+import me.raindance.champions.kits.enums.InvType;
+import me.raindance.champions.kits.enums.SkillType;
+import me.raindance.champions.kits.iskilltypes.action.IEnergy;
+import me.raindance.champions.kits.skilltypes.ChargeUp;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+@SkillMetadata(id = 1012, skillType = SkillType.Sorcerer, invType = InvType.SWORD)
+public class Gust extends ChargeUp implements IEnergy {
+    private final float duration = 4;           //  duration of slow
+
+    private final int freq = 5;                 // Frequency of particles
+    private final float rate = 0.075f;          // Rate of charge
+
+    private final float cooldown = 10;
+    private final int energy = 40;
+
+    private final double userPushMult = 0.75;
+    private final double userVertBoost = 0.25;
+
+    private final double enemyPushMult = 1.5;
+    private final double enemyVertBoost = 0.6;
+
+    @Override
+    public float getRate() {
+        return rate;
+    }
+
+    @Override
+    public void release() {
+        useEnergy();
+        Vector enemyPush = getPlayer().getEyeLocation().getDirection().normalize().multiply(enemyPushMult * getCharge());
+        enemyPush.setY(getPlayer().getEyeLocation().getDirection().normalize().getY() + enemyVertBoost);
+
+        Vector userPush = getPlayer().getEyeLocation().getDirection().normalize().multiply(-(userPushMult * getCharge()));
+        userPush.setY(userPush.getY() + userVertBoost);
+
+        getPlayer().setVelocity(userPush);
+        WrapperPlayServerWorldParticles particlePlayer = ParticleGenerator.createParticle(getPlayer().getLocation().toVector(), EnumWrappers.Particle.EXPLOSION_LARGE, 1, 0,0,0);
+        PacketUtil.asyncSend(particlePlayer, getPlayers());
+
+        Location effectLocation = getPlayer().getEyeLocation().add(getPlayer().getEyeLocation().getDirection().normalize().multiply(2));
+
+        for (Player p : BlockUtil.getPlayersInArea(effectLocation, 3, getPlayers())) {
+            if (p == getPlayer()) continue;
+            if(!isAlly(p)) {
+                StatusApplier applier = StatusApplier.getOrNew(p);
+                applier.applyStatus(Status.SLOW, duration, 0, true, true);
+            }
+            p.setVelocity(enemyPush);
+            createEffect(getPlayer().getEyeLocation(), p.getEyeLocation());
+            DamageApplier.damage(p, getPlayer(), 0, this, false);
+        }
+
+        SoundPlayer.sendSound(getPlayer().getLocation(), "mob.wither.shoot", 0.4F, 88);
+
+    }
+
+    @Override
+    public float getCooldown() {
+        return cooldown;
+    }
+
+    @Override
+    public String getName() {
+        return "Gust";
+    }
+
+    @Override
+    public int getEnergyUsage() {
+        return energy;
+    }
+
+    private void createEffect(Location start, Location end) {
+        Vector cur = start.toVector();
+        Vector inc = VectorUtil.fromAtoB(start.toVector(), end.toVector()).divide(new Vector(freq, freq, freq));
+        for(int i = 0; i < freq; i++) {
+            WrapperPlayServerWorldParticles particle = ParticleGenerator.createParticle(cur, EnumWrappers.Particle.EXPLOSION_NORMAL, 1, 0,0,0);
+            PacketUtil.asyncSend(particle, getPlayers());
+            cur.add(inc);
+        }
+
+    }
+}
