b6f4091611120fee9d151b58b253dc2e242b929f
rain474 <lordraindance5@gmail.com>
2020-04-09 18:48:09 -0400

    slight refactor to make it readable

--- src/main/java/me/raindance/champions/kits/skills/warden/Adrenaline.java
+++ src/main/java/me/raindance/champions/kits/skills/warden/Adrenaline.java
@@ -2,6 +2,7 @@ package me.raindance.champions.kits.skills.warden;
 
 import com.podcrash.api.mc.damage.Cause;
 import com.podcrash.api.mc.events.DamageApplyEvent;
+import com.podcrash.api.mc.util.EntityUtil;
 import me.raindance.champions.kits.enums.InvType;
 import me.raindance.champions.kits.enums.ItemType;
 import me.raindance.champions.kits.enums.SkillType;
@@ -26,7 +27,7 @@ public class Adrenaline extends Passive {
     @EventHandler
     public void damage(DamageApplyEvent e) {
         if(e.getAttacker() != getPlayer()) return;
-        if(getPlayer().getHealth()/getPlayer().getMaxHealth() <= 0.4D && e.getCause().equals(Cause.MELEE)) {
+        if(EntityUtil.isBelow(getPlayer(), 0.4) && e.getCause().equals(Cause.MELEE)) {
             e.setVelocityModifierX(e.getVelocityModifierX() * 1.33);
             e.setVelocityModifierZ(e.getVelocityModifierZ() * 1.33);
             e.addSource(this);
