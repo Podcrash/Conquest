4d0f8bb16cf7b6bb09f295635a730b8ccd40e15e
rain474 <lordraindance5@gmail.com>
2020-05-25 18:30:59 -0400

    restrict block place/break

--- src/main/java/me/raindance/champions/listeners/maintainers/RestrictPickup.java
+++ src/main/java/me/raindance/champions/listeners/maintainers/RestrictPickup.java
@@ -3,6 +3,8 @@ package me.raindance.champions.listeners.maintainers;
 import com.podcrash.api.listeners.ListenerBase;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,5 +16,19 @@ public class RestrictPickup extends ListenerBase {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void stopPickup(PlayerPickupItemEvent e) {
         e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void stopBreak(BlockBreakEvent e) {
+        e.setCancelled(true);
+        if (!e.getPlayer().hasPermission("invicta.map"))
+            e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void stopPlace(BlockPlaceEvent e) {
+        e.setCancelled(true);
+        if (!e.getPlayer().hasPermission("invicta.map"))
+            e.setCancelled(true);
     }
 }
