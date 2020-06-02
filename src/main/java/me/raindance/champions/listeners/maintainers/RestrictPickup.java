eb22a81d01bcd2bcd74156dd12185d0f3e9e9932
rain474 <lordraindance5@gmail.com>
2020-06-02 01:36:05 -0400

    relocated game specific restrictions

--- src/main/java/me/raindance/champions/listeners/maintainers/RestrictPickup.java
+++ src/main/java/me/raindance/champions/listeners/maintainers/RestrictPickup.java
@@ -5,7 +5,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RestrictPickup extends ListenerBase {
@@ -31,4 +36,23 @@ public class RestrictPickup extends ListenerBase {
         if (!e.getPlayer().hasPermission("invicta.map"))
             e.setCancelled(true);
     }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void preventCraft(InventoryClickEvent event) {
+        Inventory clicked = event.getClickedInventory();
+        if (clicked != null && clicked.getType() == InventoryType.CRAFTING)
+            event.setCancelled(true);
+    }
+
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onDrop(PlayerDropItemEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onFood(FoodLevelChangeEvent e) {
+        e.setCancelled(true);
+    }
+
 }
